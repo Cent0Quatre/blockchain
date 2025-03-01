13ee7f5299a756feefd74bf89d308adde133264d
Cent0Quatre <133564178+Cent0Quatre@users.noreply.github.com>
2025-03-01 14:11:59 +0100

    Update test_btc.py

--- test_btc.py
+++ test_btc.py
@@ -1,36 +1,8 @@
 from bitcoin import Blockchain, Wallet
 
-# Exemple d'utilisation
-def run_simulation():
-    blockchain = Blockchain(difficulty=4)
-    
-    # Créer quelques portefeuilles
-    alice = Wallet()
-    bob = Wallet()
-    charlie = Wallet()
-    
-    print(f"Alice: {alice.public_key[:10]}...")
-    print(f"Bob: {bob.public_key[:10]}...")
-    print(f"Charlie: {charlie.public_key[:10]}...")
-    
-    # Des transactions
-    tx1 = alice.create_transaction(bob.public_key, 1.0)
-    blockchain.add_transaction_to_mempool(tx1)
-
-    tx2 = bob.create_transaction(alice.public_key, 0.5)
-    blockchain.add_transaction_to_mempool(tx2)
-
-    # Charlie mine un bloc
-    print("\nCharlie mine un bloc...")
-    blockchain.mine_pending_transactions(charlie.public_key)
-
-    print(len(blockchain.chain[1].transactions))
-    
-    # Vérifier la validité de la blockchain
-    print(f"\nLa blockchain est-elle valide? {blockchain.is_chain_valid()}")
-    
-    # Afficher les blocs
-    print("\nBlocs dans la blockchain:")
+def chain_print(blockchain: Blockchain) -> None:
+    """Affiche la bitcoin_bc et le contenu des blocs."""
+    print("\nBlocs dans la bitcoin_bc:")
     for block in blockchain.chain:
         print(f"Bloc #{block.index} | Hash: {block.hash[:10]}... | Transactions: {len(block.transactions)}")
         for tx in block.transactions:
@@ -38,6 +10,40 @@ def run_simulation():
                 print(f"  - Récompense de minage pour {tx.recipient[:10]}...: {tx.amount} BTC")
             else:
                 print(f"  - {tx.sender[:10]}... envoie {tx.amount} BTC à {tx.recipient[:10]}...")
+
+# Exemple d'utilisation
+def run_simulation():
+    bitcoin_bc = Blockchain(difficulty=4)
+    
+    # Créer quelques portefeuilles
+    Yann = Wallet()
+    Moustafa = Wallet()
+    Bouldingue = Wallet()
+    
+    print(f"Yann: {Yann.public_key[:10]}...")
+    print(f"Moustafa: {Moustafa.public_key[:10]}...")
+    
+    # Des transactions
+    tx1 = Yann.create_transaction(Moustafa.public_key, 10)
+    bitcoin_bc.add_transaction_to_mempool(tx1)
+
+    tx2 = Moustafa.create_transaction(Yann.public_key, 1.5)
+    bitcoin_bc.add_transaction_to_mempool(tx2)
+
+    print("\nYann mine un bloc...")
+    bitcoin_bc.mine_pending_transactions(Yann.public_key)
+
+    tx3 = Moustafa.create_transaction(Bouldingue.public_key, 10000.0)
+    bitcoin_bc.add_transaction_to_mempool(tx3)
+
+    for _ in range(10):
+        print("\nBouldingue mine un bloc...")
+        bitcoin_bc.mine_pending_transactions(Bouldingue.public_key)
+    
+    # Vérifier la validité de la bitcoin_bc
+    print(f"\nLa bitcoin_bc est-elle valide? {bitcoin_bc.is_chain_valid()}")
+
+    chain_print(bitcoin_bc)
 
 # Simulation
 if __name__ == "__main__":
